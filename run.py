b8881c92a485b029554ba4fa375f5d0b80029d58
Vincent Jousse <vincent@jousse.org>
2015-11-08 00:35:16 +0100

    First commit

---
+++ run.py
@@ -0,0 +1,37 @@
+import asyncio
+import aiopg
+import psycopg2
+
+dsn = 'dbname=viserlalune user=*** password=*** host=127.0.0.1'
+
+@asyncio.coroutine
+def go():
+    pool = yield from aiopg.create_pool(dsn)
+    with (yield from pool.cursor(cursor_factory=psycopg2.extras.DictCursor)) as cur:
+        yield from cur.execute("SELECT slug, content, title, publish_date FROM blog_blogpost")
+        rows = yield from cur.fetchall()
+        for row in rows:
+            print("{} - {} | {}".format(row['publish_date'], row['slug'], row['title']))
+            with open("{}.md".format(row['slug']), "w") as f:
+                content = """<!-- 
+.. title: {title}
+.. slug: {slug}
+.. date: {date}
+.. tags:
+.. category: 
+.. link: 
+.. description: 
+.. type: text
+-->
+
+{text}
+"""
+                f.write(content.format(
+                    title=row['title'], 
+                    slug=row['slug'], 
+                    date=row['publish_date'],
+                    text=row['content']
+                ))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(go())
