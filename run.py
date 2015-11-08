2f55d111be97b0b1103a2805d66474221db11a94
Vincent Jousse <vincent@jousse.org>
2015-11-08 22:08:43 +0100

    Modify select to join categories

--- run.py
+++ run.py
@@ -5,20 +5,19 @@ import os
 
 from config import dsn
 
-@asyncio.coroutine
-def go():
+select = """
+SELECT 
+    p.slug as pslug, p.content, p.title as ptitle, p.publish_date, c.slug as cslug, c.title as ctitle
+FROM blog_blogpost 
+AS p
+JOIN blog_blogpost_categories
+ON blog_blogpost_categories.blogpost_id = p.id
+JOIN blog_blogcategory
+AS c
+ON c.id = blog_blogpost_categories.blogcategory_id
+"""
 
-    if not os.path.exists('posts'):
-        os.makedirs('posts')
-
-    pool = yield from aiopg.create_pool(dsn)
-    with (yield from pool.cursor(cursor_factory=psycopg2.extras.DictCursor)) as cur:
-        yield from cur.execute("SELECT slug, content, title, publish_date FROM blog_blogpost")
-        rows = yield from cur.fetchall()
-        for row in rows:
-            print("{} - {} | {}".format(row['publish_date'], row['slug'], row['title']))
-            with open("posts/{}.md".format(row['slug']), "w") as f:
-                content = """<!-- 
+content = """<!-- 
 .. title: {title}
 .. slug: {slug}
 .. date: {date}
@@ -31,10 +30,25 @@ def go():
 
 {text}
 """
-                
+
+@asyncio.coroutine
+def go():
+
+    if not os.path.exists('posts'):
+        os.makedirs('posts')
+
+    pool = yield from aiopg.create_pool(dsn)
+    with (yield from pool.cursor(cursor_factory=psycopg2.extras.DictCursor)) as cur:
+        yield from cur.execute(select)
+        rows = yield from cur.fetchall()
+        curr = None
+        for i, row in enumerate(rows):
+
+            print("{} - {} | {}".format(row['publish_date'], row['pslug'], row['ptitle']))
+            with open("posts/{}.md".format(row['pslug']), "w") as f:
                 f.write(content.format(
-                    title=row['title'], 
-                    slug=row['slug'], 
+                    title=row['ptitle'], 
+                    slug=row['pslug'], 
                     date=row['publish_date'],
                     text=row['content']
                 ))
